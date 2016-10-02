a41e7494c72229f9dbacbf9433a62f0e158e5085
Sam Saccone <samccone@gmail.com>
2016-10-02 12:54:00 -0700

    woot

---
+++ webapp/src/index.ts
@@ -0,0 +1,87 @@
+class Handle {
+  private elm: HTMLElement;
+  private track: HTMLElement;
+  private boundingElm: HTMLElement;
+  private boundingHeight: number;
+  private boundingTopOffset: number;
+  private dragging: Boolean = false;
+  private handlePercent: number = 0.5;
+
+  constructor() {
+    this.elm = document.querySelector('.handle') as HTMLElement;
+    this.track = document.querySelector('.track') as HTMLElement;
+    this.boundingElm = document.querySelector('.value-selector') as HTMLElement;
+
+
+    this.updateBounds();
+    this.setEventListeners();
+    this.updateHandlePosition();
+  }
+
+
+  private setEventListeners() {
+    this.elm.addEventListener('mousedown', e => this.onMouseDown(e as MouseEvent));
+    this.track.addEventListener('mousedown', e => this.onTrackDown(e as MouseEvent));
+    document.body.addEventListener('mousemove', e => this.onMouseMove(e as MouseEvent));
+    document.body.addEventListener('mouseleave', e => this.onMouseUp(e as MouseEvent));
+    document.body.addEventListener('mouseup', e => this.onMouseUp(e as MouseEvent));
+
+    this.elm.addEventListener('touchstart', e => this.onTouchDown(e as TouchEvent));
+    this.track.addEventListener('touchstart', e => this.onTrackTouchDown(e as TouchEvent));
+    document.body.addEventListener('touchend', e => this.onMouseUp(e as TouchEvent));
+    document.body.addEventListener('touchmove', e => this.onTouchMove(e as TouchEvent));
+
+    window.addEventListener('resize', e => {
+      this.updateBounds();
+      this.updateHandlePosition();
+    });
+  }
+
+  private updateBounds() {
+    let rect = this.boundingElm.getBoundingClientRect();
+    this.boundingHeight = rect.height;
+    this.boundingTopOffset = rect.top;
+  }
+
+  private updateHandlePosition() {
+    let nextY = this.boundingHeight * this.handlePercent;
+    this.elm.style.top = `${nextY}px`;
+  }
+
+  private onMouseMove(e: {pageY: number}) {
+    if (this.dragging === false) {
+      return;
+    }
+
+    this.handlePercent = Math.min(1, Math.max(0, (e.pageY - this.boundingTopOffset) / this.boundingHeight));
+    this.updateHandlePosition();
+  }
+
+  private onTouchDown(e: TouchEvent) {
+    this.onMouseDown(e);
+  }
+
+  private onTrackTouchDown(e: TouchEvent) {
+   this.onTrackDown({pageY: e.touches[0].pageY});
+  }
+
+  private onTouchMove(e: TouchEvent) {
+    e.preventDefault();
+    this.onMouseMove({pageY: e.touches[0].pageY});
+  }
+
+  private onTrackDown(e: {pageY: number}) {
+    this.onMouseDown(e);
+    this.onMouseMove(e);
+  }
+
+  private onMouseDown(e: any) {
+    this.dragging = true;
+  }
+
+  private onMouseUp(e: any) {
+    this.dragging = false;
+  }
+}
+
+new Handle();
